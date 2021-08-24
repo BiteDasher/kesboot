7e9ee1ea2c1bd1038699f598fe7dc43e47cc198a
BiteDasher <bitedasher@bite.ru>
2021-08-24 13:29:55 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 __install() {
-	sudo install -Dm755 ./kesboot /usr/local/bin/kesboot
-	sudo install -Dm644 ./main.sh /usr/share/kesboot/main.sh
-	sudo install -Dm644 ./default /usr/share/kesboot/default
-	sudo install -Dm644 ./kesboot.conf /etc/kesboot.conf
-	sudo install -Dm755 ./hook /usr/share/libalpm/scripts/kesboot-hook
-	sudo install -Dm644 ./pacman-hook /usr/share/libalpm/hooks/99-update-kesboot
+	sudo install -vDm755 ./kesboot /usr/local/bin/kesboot
+	sudo install -vDm644 ./main.sh /usr/share/kesboot/main.sh
+	sudo install -vDm644 ./default /usr/share/kesboot/default
+	sudo install -vDm644 ./kesboot.conf /etc/kesboot.conf
+	[ "$PACMAN" == 1 ] && sudo install -vDm755 ./hook /usr/share/libalpm/scripts/kesboot-hook
+	[ "$PACMAN" == 1 ] && sudo install -vDm644 ./pacman-hook /usr/share/libalpm/hooks/99-update-kesboot
 }
 if [ -z "$1" ]; then
 	__install
@@ -14,9 +14,10 @@ case "$1" in
 	install) __install ;;
 	remove) sudo rm -r -v /usr/local/bin/kesboot \
 			      /usr/share/kesboot \
-			      /etc/kesboot.conf \
-			      /usr/share/libalpm/scripts/kesboot-hook \
-			      /usr/share/libalpm/hooks/99-update-kesboot ;;
+			      /etc/kesboot.conf $(
+			      if [ "$PACMAN" == 1 ]; then echo '
+				/usr/share/libalpm/scripts/kesboot-hook
+				/usr/share/libalpm/hooks/99-update-kesboot'; fi) ;;
 	makepkg) 
 		install -Dm755 ./kesboot "$pkgdir"/usr/local/bin/kesboot
 		install -Dm644 ./main.sh "$pkgdir"/usr/share/kesboot/main.sh
